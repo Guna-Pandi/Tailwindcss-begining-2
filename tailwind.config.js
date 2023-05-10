a90edde2c6835f83c31e8f0290a52ae939e9b440
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-10 12:38:09 +0530

    Inilitialized:Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "color-primary":'#01501e',
+        "color-primary-light":'#020726',
+        "color-primary-dark":'#010417',
+        "color-secondary":'#ff7d3b',
+        "color-gray":'#333',
+        "color-white":'#fff',
+        "color-blob":'#A427DF',
+      }
+    },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'20px',
+        md:'50px'
+      }
+    }
   },
   plugins: [],
 }
