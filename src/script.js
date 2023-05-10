6aca7ab51b006ab5678778e63cb867d3e96885a6
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-11 00:32:18 +0530

    Added:Testimonials section

--- src/script.js
+++ src/script.js
@@ -1,17 +1,36 @@
+// mobile menu
 const Hamburger = document.querySelector("#hamburger");
 const Menu = document.querySelector("#menu");
-const Hlink = document.querySelector("#hlink");
+const Hlink = document.querySelectorAll("#hlink");
 const FaBars = document.querySelector(".fa-bars");
 
-Hamburger.addEventListener('click', () => {
-    Menu.classList.toggle('hidden');
-    FaBars.classList.toggle('fa-xmark');
-  });
-  
-  Hlink.forEach(link => {
-    link.addEventListener('click',()=>{
-        Menu.classList.toggle('hidden');
-    FaBars.classList.toggle('fa-xmark');
+Hamburger.addEventListener("click", () => {
+  Menu.classList.toggle("hidden");
+  FaBars.classList.toggle("fa-xmark");
+});
 
-    })
+Hlink.forEach((link) => {
+  link.addEventListener("click", () => {
+    Menu.classList.toggle("hidden");
+    FaBars.classList.toggle("fa-xmark");
   });
+});
+
+// Testimonials
+
+const userTexts = document.getElementsByClassName("user-text");
+const userPics = document.getElementsByClassName("user-pic");
+
+function showReview() {
+  for (userPic of userPics) {
+    userPic.classList.remove("active-pic");
+  }
+  for (userText of userTexts) {
+    userText.classList.remove("active-text");
+  }
+
+  let i = Array.from(userPics).indexOf(event.target);
+
+  userPics[i].classList.add("active-pic");
+  userTexts[i].classList.add("active-text");
+}
