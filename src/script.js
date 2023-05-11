f5e14267ec504d9c777ef8921f1e51caac856c32
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-11 18:20:46 +0530

    Added:Blog section

--- src/script.js
+++ src/script.js
@@ -35,8 +35,8 @@ function showReview() {
   userTexts[i].classList.add("active-text");
 }
 
-
-const toggleBtn = document.getElementById('toggleBtn');
+// pricin toggle function
+const toggleBtn = document.getElementById("toggleBtn");
 
 const card_1_front = document.querySelector("#card_1_front");
 const card_1_back = document.querySelector("#card_1_back");
@@ -47,14 +47,13 @@ const card_2_back = document.querySelector("#card_2_back");
 const card_3_front = document.querySelector("#card_3_front");
 const card_3_back = document.querySelector("#card_3_back");
 
-toggleBtn.addEventListener('change', () => {
-    card_1_front.classList.toggle('-rotate-y-180');
-    card_1_back.classList.toggle('rotate-y-180');
+toggleBtn.addEventListener("change", () => {
+  card_1_front.classList.toggle("-rotate-y-180");
+  card_1_back.classList.toggle("rotate-y-180");
 
-    card_2_front.classList.toggle('-rotate-y-180');
-    card_2_back.classList.toggle('rotate-y-180');
+  card_2_front.classList.toggle("-rotate-y-180");
+  card_2_back.classList.toggle("rotate-y-180");
 
-    card_3_front.classList.toggle('-rotate-y-180');
-    card_3_back.classList.toggle('rotate-y-180');
-    
-})
+  card_3_front.classList.toggle("-rotate-y-180");
+  card_3_back.classList.toggle("rotate-y-180");
+});
